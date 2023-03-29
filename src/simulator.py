789876f843c92c5ff9bbb46b090db96c51ebc3b0
Kincent Lan <klanlincoln@gmail.com>
2023-03-29 18:45:51 -0400

    Finished simulator

--- src/simulator.py
+++ src/simulator.py
@@ -8,7 +8,13 @@ env.launch(realtime=True)
 
 # Create a puma in the default zero pose
 puma = rtb.models.Puma560()
+
+pumaDH = rtb.models.DH.Puma560()
+# puma.qz = np.array( [ 2.6486,     -2.38986263,  2.98768081,  0.76815354,  0.86615253,  2.5455596 ])
 puma.q = puma.qz
+
+print(f'Puma: {puma}')
+
 env.add(puma, robot_alpha=True, collision_alpha=False)
 
 dt = 0.05
@@ -21,6 +27,7 @@ poses = [puma.qz, puma.rd, puma.ru, puma.lu, puma.ld]
 for previous, target in zip(poses[:-1], poses[1:]):
     for alpha in np.linspace(0.0, 1.0, int(interp_time / dt)):
         puma.q = previous + alpha * (target - previous)
+        print(f'Puma.q: {pumaDH.itorque(puma.q, np.ones((6,)))}')
         env.step(dt)
     for _ in range(int(wait_time / dt)):
         puma.q = target
