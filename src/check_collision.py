6c0e239103cdd1e67a62526a76933132d8408502
SamDaQueen <samreenreyaz@outlook.com>
2023-04-13 18:18:42 -0400

    Working collision check for given q and spheres

--- src/check_collision.py
+++ src/check_collision.py
@@ -1,32 +1,22 @@
 import numpy as np
 
 
-def check_collision(robot, q, link_radius, sphere_centers, sphere_radii, resolution=11):
-    x1 = np.zeros(3)
-    T2 = robot.fkine(q)[:3, :3]  # homogenous transform
-    x2 = T2.t  # The translation
-    T3 = T2 @ robot.fkine(q, 3)[:3, :3]
-    x3 = T3.t
-
-    if resolution is None:
-        resolution = 11
-    ticks = np.linspace(0, 1, resolution)  # 11 evenly spaced points between 0 and 1
-    n = len(ticks)
-    # x12 -> n points on straight line from origin to joint 3
-    x12 = np.tile(x1, (n, 1)) + np.tile(x2 - x1, (n, 1)) * np.tile(ticks, (3, 1)).T
-    # x13 -> n points on straight line on last arm
-    x23 = np.tile(x2, (n, 1)) + np.tile(x3 - x2, (n, 1)) * np.tile(ticks, (3, 1)).T
-    # all the points
-    points = np.concatenate([x12, x23], axis=0)
-
+def check_collision(robot, link_radius, q, sphere_centers, sphere_radii, resolution=5):
+    links = robot.links
     in_collision = False
-    for i in range(sphere_centers.shape[0]):  # for each sphere
-        # Find L2 distance from each point found above with center of
-        # sphere.
-        # check if any distance > sum od radii of sphere and link
-        if np.any(np.sum((points - np.tile(sphere_centers[i, :], (points.shape[0], 1)).T) ** 2, axis=0) < (
-                link_radius + sphere_radii[i]) ** 2):
-            in_collision = True
-            break
+    fkine = robot.fkine_all(q)
+
+    # Define the positions of interest for each link
+    positions = [np.linspace(fkine[i].t, fkine[i + 1].t, resolution) for i in [0, 1, 2, 3]]
+
+    # Check for collisions at each position
+    for i, link_pos in enumerate(positions):
+        for j, pos in enumerate(link_pos):
+            for k, sphere_center in enumerate(sphere_centers):
+                dist = np.linalg.norm(pos - sphere_center)
+                if dist <= (link_radius + sphere_radii[k]):
+                    print("Collision detected at link {} position {} with sphere {}!".format(i, j, k))
+                    in_collision = True
+                    return in_collision
 
     return in_collision
--- src/test.py
+++ src/test.py
@@ -8,19 +8,23 @@ robot = models.DH.Puma560()
 q_min = [-175, -90, -150, -190, -120, -360]
 q_max = [175, 85, 60, 190, 120, 360]
 
+colliding_sphere = robot.fkine(robot.q).t
+
 link_radius = 0.05
 sphere_centers = np.array([
     [0.5, 0, 0],
     [0, 0.5, 0],
     [0, 0, 0.5]
+    colliding_sphere
 ])
-sphere_radii = np.array([0.1, 0.1, 0.1])
+
+sphere_radii = np.array([0.1, 0.1, 0.1, 0.1])
 
 # Generate random configuration
-q = np.random.uniform(q_min, q_max, size=6)
+# q = np.random.uniform(q_min, q_max, size=6)
 
 # Check for collisions
-in_collision = check_collision(robot, q, link_radius, sphere_centers, sphere_radii)
+in_collision = check_collision(robot, link_radius, robot.q, sphere_centers, sphere_radii)
 
 if in_collision:
     print("Robot is in collision!")
