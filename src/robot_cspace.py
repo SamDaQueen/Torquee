692c7a0f6a3d9d9cd679d381b395ee604e2e6a5b
SamDaQueen <samreenreyaz@outlook.com>
2023-04-13 22:59:25 -0400

    Valid neighbors in 26 directions

--- src/robot_cspace.py
+++ src/robot_cspace.py
@@ -3,57 +3,106 @@ import numpy as np
 
 class RobotCSpace:
     def __init__(self, joint_limits, step_size):
+        """
+        Initializes the configuration space of the robot.
+
+        :param joint_limits: The joint limits of the robot
+        :param step_size: The step size of the configuration space
+        """
         self.joint_limits = joint_limits
         self.step_size = step_size
         self.num_cells = [int(np.ceil((limit[1] - limit[0]) / step_size)) + 1 for limit in joint_limits]
-        self.grid = self.create_grid()
+        self.grid = np.ones(self.num_cells, dtype=bool)
 
-    def create_grid(self):
+    def get_grid(self):
+        """
+        Returns the grid of the configuration space
 
-        grid = np.zeros(self.num_cells, dtype=bool)
-        for i, limit in enumerate(self.joint_limits):
-            # Calculate grid indices corresponding to joint limits
-            lower_index = int(np.round((limit[0] % 360) / self.step_size))
-            upper_index = int(np.round((limit[1] % 360) / self.step_size))
-
-            # Set corresponding grid cells to True
-            if upper_index >= lower_index:
-                grid[(slice(None),) * i + tuple(range(lower_index, upper_index + 1))] = True
-            else:
-                grid[(slice(None),) * i + tuple(range(lower_index, self.num_cells[i]))] = True
-                grid[(slice(None),) * i + tuple(range(upper_index + 1))] = True
-        return grid
+        :return: the grid
+        """
+        return self.grid
 
     def set_config_value(self, config, value):
-        index = tuple(int(np.round((angle % 360) / step_size)) for angle in config)
+        """
+        Sets the value of a configuration in the grid
+
+        :param config: The configuration in joint angles
+        :param value: True or False
+        """
+        index = self.convert_config_to_cell(config)
+        # return if config is invalid
+        if not self.is_valid(config):
+            return
         self.grid[index] = value
 
+    def convert_config_to_cell(self, config):
+        """
+        Converts a configuration to a cell index in the grid
+
+        :param config:  The configuration in joint angles
+        :return: The cell index
+        """
+        # Convert cell index to 6D coordinate
+        return [int((coord - limit[0]) / self.step_size) for coord, limit in zip(config, self.joint_limits)]
+
+    def convert_cell_to_config(self, cell):
+        """
+        Converts a cell index in the grid to a configuration in joint angles
+
+        :param cell:    The cell index
+        :return:    The configuration in joint angles
+        """
+        # Convert 6D coordinate to cell index
+        return [coord * self.step_size + limit[0] for coord, limit in zip(cell, self.joint_limits)]
+
     def find_neighbors(self, config):
-        neighbors = []
-        for i in range(len(config)):
-            for delta in [-1, 0, 1]:
-                neighbor_config = config.copy()
-                neighbor_config[i] += delta * self.step_size
-                if np.all(neighbor_config >= 0) and np.all(neighbor_config < 360):
-                    index = tuple(int(np.round((angle % 360) / self.step_size)) for angle in neighbor_config)
-                    if self.grid[index]:
-                        neighbors.append(neighbor_config)
-        return neighbors
+        """
+        Finds the neighbors of a configuration in the grid
+
+        :param config:  The configuration in joint angles
+        :return:    A list of neighbors in joint angles
+        """
+        # return if config is invalid
+        if not self.is_valid(config):
+            return []
+
+        coord = self.convert_config_to_cell(config)
+
+        # generate list of possible neighbor coordinates
+        valid_neighbors = []
+        for i in range(6):
+            for offset in [-1, 0, 1]:
+                neighbor_index = tuple(coord[j] + (offset if i == j else 0) for j in range(6))
+
+                if neighbor_index != tuple(coord) and all(
+                        0 <= neighbor_index[j] < self.num_cells[j] for j in range(6)) and \
+                        self.grid[neighbor_index]:
+                    valid_neighbors.append(neighbor_index)
+
+        # convert neighbor coordinates to joint angles
+        valid_neighbors = [self.convert_cell_to_config(neighbor) for neighbor in valid_neighbors]
+
+        return valid_neighbors
+
+    def is_valid(self, config):
+        """
+        Checks if a configuration is valid
+
+        :param config:  The configuration in joint angles
+        :return:    True if valid, False otherwise
+        """
+        index = self.convert_config_to_cell(config)
+        return all(0 <= i < n for i, n in zip(index, self.grid.shape))
 
 
 if __name__ == '__main__':
-    joint_limits = [(0, 360)] * 6
+    q_min = [-175, -90, -150, -190, -120, -360]
+    q_max = [175, 85, 60, 190, 120, 360]
+    # joint_limits = [(0, 30)] * 6
+    joint_limits = list(zip(q_min, q_max))
+
     step_size = 10
     config_space = RobotCSpace(joint_limits, step_size)
 
-    joint_angles = [0, 40, 270, 350, 0, 0]
-    valid = config_space.grid[tuple(int(np.round((angle % 360) / step_size)) for angle in joint_angles)]
-    print(valid)
-
-    neighbors = config_space.find_neighbors(np.array([0, 40, 270, 350, 0, 0]))
+    neighbors = config_space.find_neighbors(np.array([0, 0, 0, 0, 0, 0]))
     print(neighbors)
-
-    config_space.set_config_value([0, 40, 270, 350, 0, 0], False)
-
-    valid = config_space.grid[tuple(int(np.round((angle % 360) / step_size)) for angle in joint_angles)]
-    print(valid)
